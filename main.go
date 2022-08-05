ff4d3fdf309d0a2273649c45befb00adf4bcb441
navid.shariaty <navid.shariaty@snapp.cab>
2022-08-05 13:37:28 +0430

    add imagePullPolicy

--- main.go
+++ main.go
@@ -94,6 +94,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "AgeSecret")
 		os.Exit(1)
 	}
+	if err = (&gitopssecretsnappcloudiov1alpha1.AgeKey{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "AgeKey")
+		os.Exit(1)
+	}
+	if err = (&gitopssecretsnappcloudiov1alpha1.AgeSecret{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "AgeSecret")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
